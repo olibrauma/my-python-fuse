0eff615759fc89a5ec25da09fbfe0a4e9a07598b
takeru1990 <k06aya4@gmail.com>
2024-04-13 21:57:03 +0900

    silo-fs.py は、元リポジトリの example の hello.py から再開

--- example/silo-fs.py
+++ example/silo-fs.py
@@ -22,20 +22,12 @@ if not hasattr(fuse, '__version__'):
 
 fuse.fuse_python_api = (0, 2)
 
-class File:
-    def __init__(self, name, content):
-        self.name = name
-        self.content = content
-
 # Create a dictionary and store File objects with path as key
 web = silo.get_json("/")
 pprint.pprint(web)
 
-# files 
-
-files = {}
-files["/hello"] = File("hello", b'Hello World!\n')
-files["/bye"] = File("bye", b'Goodbye!\n')
+hello_path = '/hello'
+hello_str = b'Hello World!\n'
 
 class MyStat(fuse.Stat):
     def __init__(self):
@@ -57,69 +49,37 @@ class HelloFS(Fuse):
         if path == '/':
             st.st_mode = stat.S_IFDIR | 0o755
             st.st_nlink = 2
-        elif path in files:  # Check for path in dictionary
+        elif path == hello_path:
             st.st_mode = stat.S_IFREG | 0o444
             st.st_nlink = 1
-            st.st_size = len(files[path].content)
+            st.st_size = len(hello_str)
         else:
             return -errno.ENOENT
         return st
 
     def readdir(self, path, offset):
-        for r in  '.', '..', *[f.name for f in files.values()]:
+        for r in  '.', '..', hello_path[1:]:
             yield fuse.Direntry(r)
 
     def open(self, path, flags):
-        if path not in files:
+        if path != hello_path:
             return -errno.ENOENT
-        if (flags & os.O_RDONLY) != os.O_RDONLY:
+        accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
+        if (flags & accmode) != os.O_RDONLY:
             return -errno.EACCES
-        return 0
-
 
     def read(self, path, size, offset):
-        if path not in files:
+        if path != hello_path:
             return -errno.ENOENT
-        f = files[path]  # Get matching file object from dictionary
-        slen = len(f.content)
+        slen = len(hello_str)
         if offset < slen:
             if offset + size > slen:
                 size = slen - offset
-            buf = f.content[offset:offset+size]
+            buf = hello_str[offset:offset+size]
         else:
             buf = b''
         return buf
 
-    def rename(self, old, new):
-        # Check if old path exists
-        if old not in files:
-            return -errno.ENOENT
-
-        # Check if new path already exists
-        if new in files:
-            return -errno.EEXIST
-
-        # Get the File object from old path
-        f = files[old]
-
-        # Update the dictionary with new path as key and old value
-        files[new] = f
-        del files[old]
-
-        # Success
-        return 0
-
-    def unlink(self, path):
-        # Check if path exists
-        if path not in files:
-            return -errno.ENOENT
-
-        # Delete the file object from the dictionary
-        del files[path]
-
-        # Success
-        return 0
-    
 def main():
     usage="""
 Userspace hello example
