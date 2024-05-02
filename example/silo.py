b85462894d12db12e1e3eb8e8360cffc0ee74b48
takeru1990 <k06aya4@gmail.com>
2024-05-02 12:23:13 +0900

    外身を `__silo`、中身を `__silage` と呼び替え。
    サイロに貯蔵されるものそのものずばりを指すサイレージという言葉を知った。データのバイト列をサイレージと命名する必然性を感じられてちょっと嬉しい。
    
    実体をある程度は正しく反映しつつ「農業用の貯蔵庫」のニュアンスや連想から離れない良い変数名と関数名を模索してみた。あまり生産的ではないけど、自分で開発を楽しむためには大事なことなのでね。

--- example/silo.py
+++ example/silo.py
@@ -7,14 +7,14 @@ sac = SiloAPIClient(CONFIG_PATH)
 class Silo:
     def __init__(self):
         # Start Silo API Client
-        self.__crops = sac.get_json()
-        self.__crates = {}
+        self.__silo = sac.get_json()
+        self.__silage = {}
 
     def __iter__(self):
-        return SiloIterator(self.__crops)
+        return SiloIterator(self.__silo)
     
     def stat(self, path):
-        return next(filter(lambda crop: crop["filePath"] == path, self), None)
+        return next(filter(lambda s: s["filePath"] == path, self), None)
     
     def list(self, path='/'):
         # path = '/' の場合、次の正規表現でスラッシュが連続しないようにする
@@ -22,53 +22,48 @@ class Silo:
         # '{path}/.silo' 以外の '{path}/****' を抽出する
         p = rf"^{path_}/(?!.*\.silo$)"
         
-        return list(filter(lambda crop: re.search(p, crop['filePath']) is not None, self))
+        return list(filter(lambda s: re.search(p, s['filePath']) is not None, self))
 
     def haul(self, path, size, offset):
-        path_ = hash(path)
-
-        if path_ not in self.__crates:
-            self.__crates[path_] = sac.get_file(path)
+        if path not in self.__silage:
+            self.__silage[path] = sac.get_file(path)
         
-        return self.__crates[path_][offset:offset + size]
+        return self.__silage[path][offset:offset + size]
     
-    def ditch(self, path):
+    def empty(self, path):
         if self.stat(path) is None:
             raise FileNotFoundError(f'File not found: {path}')
         else:
             sac.delete_file(path)
-            self.__crops = list(filter(lambda crop: crop["filePath"] != path, self))
+            self.__silo = list(filter(lambda s: s["filePath"] != path, self))
+            del self.__silage[path]
             return 0
 
-    def load(self, path, buf, offset):
-        path_ = hash(path)
-        
-        if path_ not in self.__crates:
-            self.__crates[path_] = b''
+    def load(self, path, buf, offset):        
+        if path not in self.__silage:
+            self.__silage[path] = b''
 
-        self.__crates[path_][offset:] += buf
+        self.__silage[path][offset:] += buf
         
         return len(buf)
     
-    def put(self, path):
-        path_ = hash(path)
-
-        if len(self.__crates[path_]) == 0:
+    def fill(self, path):
+        if len(self.__silage[path]) == 0:
             return 0
         else:
-            sac.write_file(path, self.__crates[path_])
-            del self.__crates[path_]
+            sac.write_file(path, self.__silage[path])
+            del self.__silage[path]
             return 0
 
 class SiloIterator:
-    def __init__(self, crops):
-        self.crops = crops
+    def __init__(self, silo_):
+        self.silo = silo_
         self.index = 0
     
     def __next__(self):
-        if self.index < len(self.crops):
-            crop = self.crops[self.index]
+        if self.index < len(self.silo):
+            s = self.silo[self.index]
             self.index += 1
-            return crop
+            return s
         else:
             raise StopIteration
